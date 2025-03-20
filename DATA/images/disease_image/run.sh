b5a6a34cba5f3125725a525cf46d76f774f3728d
ray <geemguang@gmail.com>
2025-03-20 21:04:34 +0900

    redo

--- DATA/images/disease_image/run.sh
+++
@@ -1,7 +0,0 @@
-
-python JsonCrop01_read_json.py &&
-python JsonCrop02_create_anno_json.py &&
-python JsonCrop03_copy_images_to_dataset.py &&
-python JsonCrop04_draw_disease_bbox.py &&
-python JsonCrop05_cropping_with_extra_bound_dataset.py &&
-python JsonCrop06_cropping_with_extra_bound_dataset.py 
--- upjab/tool/__init__.py
+++
@@ -1,2 +0,0 @@
-from .shuffle_split_folder import shuffle_split_folder
-from .what_missing_in_folder import what_missing_in_folder
