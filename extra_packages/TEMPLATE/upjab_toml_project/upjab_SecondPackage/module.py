11a3c19962a665241f57b7dc4665368cf149fa33
humorbeing <geemguang@gmail.com>
2025-04-22 08:50:54 +0900

    template package

---
+++ extra_packages/TEMPLATE/upjab_toml_project/upjab_SecondPackage/module.py
@@ -0,0 +1,13 @@
+import os
+
+
+def hello():
+    file_path = os.path.abspath(__file__)
+    file_name = os.path.basename(file_path)
+
+    print(f"The absolute path of the file is: {file_path}")
+    print(f"The name of the file is: {file_name}")
+
+
+# Call the function
+# hello()
